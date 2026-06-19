8c72302c699b4cc10f17013b00bd36b2e0bcba96
Sebastian Huber <sebastian.huber@embedded-brains.de>
2026-06-19 06:49:09 +0200

    gcov: Optimize condition coverage code generation
    Change the code to avoid emitting bitwise-or operations with a zero constant
    operand since they are effectively no-operations. The ommitted bitwise-or
    operations are potentially relaxed atomic operations which cannot be optimized
    away.
    
    For example, consider this test case:
    
    int a(int);
    int b(int);
    int g(int i)
    {
      if (i) {
        return a(i);
      } else {
        return b(i);
      }
    }
    
    On 64-bit RISC-V this change result in the following code changes for options
    -fprofile-update=atomic -fcondition-coverage-fprofile-info-section -O2:
    
      --- test.s.old        2026-06-19 06:53:06.806622709 +0200
      +++ test.s.new        2026-06-19 06:53:29.187922134 +0200
      @@ -9,15 +9,13 @@
              .type   g, @function
       g:
              lui     a5,%hi(.LANCHOR0)
      -       addi    a5,a5,%lo(.LANCHOR0)
              beq     a0,zero,.L2
              li      a4,1
      +       addi    a5,a5,%lo(.LANCHOR0)
              amoor.d zero,a4,0(a5)
      -       addi    a5,a5,8
      -       amoor.d zero,zero,0(a5)
              tail    a
       .L2:
      -       amoor.d zero,zero,0(a5)
      +       addi    a5,a5,%lo(.LANCHOR0)
              li      a4,1
              addi    a5,a5,8
              amoor.d zero,a4,0(a5)
      @@ -35,7 +33,7 @@
              .word   1110847786
              .zero   4
              .dword  0
      -       .word   -566726224
      +       .word   -566618674
              .word   903831156
              .dword  .LC0
              .dword  0
    
    gcc/ChangeLog:
    
    	* tree-profile.cc (emit_bitwise_op): Try to fold the operation to allow
    	optimizations for resulting zero constants.
    	(instrument_decisions): Do not emit bitwise-or operations with a
    	zero constant operand.
    
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

--- gcc/tree-profile.cc
+++ gcc/tree-profile.cc
@@ -43,6 +43,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "tree-nested.h"
 #include "gimplify.h"
 #include "gimple-iterator.h"
+#include "gimple-fold.h"
 #include "gimplify-me.h"
 #include "tree-cfg.h"
 #include "tree-into-ssa.h"
@@ -629,12 +630,27 @@ emit_assign (edge e, tree rhs)
   return emit_assign (e, make_ssa_name (gcov_type_node), rhs);
 }
 
-/* Emit LHS = OP1 <OP> OP2 on edges.  */
-tree
+/* Emit/fold OP1 <OP> [OP2] on edge E.
+   Return folded constant or SSA name.  */
+static tree
 emit_bitwise_op (edge e, tree op1, tree_code op, tree op2 = NULL_TREE)
 {
+  gimple_seq seq = NULL;
+  tree rhs;
+
+  if (op2 == NULL_TREE)
+    rhs = gimple_build (&seq, op, gcov_type_node, op1);
+  else
+    rhs = gimple_build (&seq, op, gcov_type_node, op1, op2);
+
+  if (seq)
+    gsi_insert_seq_on_edge (e, seq);
+
+  if (is_gimple_min_invariant (rhs) || TREE_CODE (rhs) == SSA_NAME)
+    return rhs;
+
   tree lhs = make_ssa_name (gcov_type_node);
-  gassign *w = gimple_build_assign (lhs, op, op1, op2);
+  gassign *w = gimple_build_assign (lhs, rhs);
   gsi_insert_on_edge (e, w);
   return lhs;
 }
@@ -1174,6 +1190,8 @@ instrument_decisions (array_slice<basic_block> expr, size_t condno,
 	  /* _global_true |= _true, _global_false |= _false  */
 	  for (size_t k = 0; k != 2; ++k)
 	    {
+	      if (integer_zerop (next[k]))
+		continue;
 	      tree ref = tree_coverage_counter_ref (GCOV_COUNTER_CONDS,
 						    2 * condno + k);
 	      if (atomic)
